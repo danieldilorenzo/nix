c792c98297d78ad9dc2ec63d08d86c32728b2cd0
Daniel Di Lorenzo <70811733+danieldilorenzo@users.noreply.github.com>
2024-09-09 14:50:30 -0300

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -105,7 +105,12 @@
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
+  
+  # Search in
+  # https://search.nixos.org/packages
+    
   environment.systemPackages = with pkgs; [
+    pkgs.zsh
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   ];
