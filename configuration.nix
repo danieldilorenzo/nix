f10417d7a3ae70036b8a6d7e599a31c3219fe2e0
Daniel Di Lorenzo <70811733+danieldilorenzo@users.noreply.github.com>
2024-10-04 10:56:31 -0300

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -111,6 +111,7 @@
     
   environment.systemPackages = with pkgs; [
     pkgs.zsh
+    pkgs.isoimagewriter
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   ];
